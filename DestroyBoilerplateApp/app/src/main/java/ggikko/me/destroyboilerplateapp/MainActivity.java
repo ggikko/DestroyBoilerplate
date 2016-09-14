c06016869c354c23c0d1cc42961207cd33cd27f1
Jihong Park <ggikko@hotmail.com>
2016-09-14 18:03:02 +0900

    init

---
+++ DestroyBoilerplateApp/app/src/main/java/ggikko/me/destroyboilerplateapp/MainActivity.java
@@ -0,0 +1,21 @@
+package ggikko.me.destroyboilerplateapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+  @BindView(R.id.hello) TextView hello;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    ButterKnife.bind(this);
+  }
+}
