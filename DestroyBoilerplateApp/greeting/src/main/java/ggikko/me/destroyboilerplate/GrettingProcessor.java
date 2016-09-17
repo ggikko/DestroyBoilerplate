3f0323ac476f3a34f97ea36fc303edee043c4672
Jihong Park <ggikko@hotmail.com>
2016-09-17 17:39:20 +0900

    add error messager

--- DestroyBoilerplateApp/greeting/src/main/java/ggikko/me/destroyboilerplate/GrettingProcessor.java
+++ DestroyBoilerplateApp/greeting/src/main/java/ggikko/me/destroyboilerplate/GrettingProcessor.java
@@ -4,23 +4,28 @@ import java.util.Collections;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
 
 /**
  * Created by ggikko on 2016. 9. 14..
  */
 public class GrettingProcessor extends AbstractProcessor {
 
+  private Messager messager;
+
   /**
    * Messager, Filter, ElementUtils, TypeUtils, SourceVersion 접근가능, with ProcessingEnvironment
-   * @param processingEnv
    */
   @Override
   public synchronized void init(ProcessingEnvironment processingEnv) {
+    messager = processingEnv.getMessager();
     super.init(processingEnv);
   }
 
@@ -28,12 +33,11 @@ public class GrettingProcessor extends AbstractProcessor {
    * 예를 들어 자바 7을 사용하면 따로
    * @SupportedSourceVersion
    * @SupportedAnnotationTypes
-   * 를 명시해줘야함
+   * 를 명시해서 아래의 메소드를 없앨 수 있음.
    */
 
   /**
    * 다루어야할 annotation 타입 정의
-   * @return
    */
   @Override
   public Set<String> getSupportedAnnotationTypes() {
@@ -42,7 +46,6 @@ public class GrettingProcessor extends AbstractProcessor {
 
   /**
    * 특정 자바 버전 명시
-   * @return
    */
   @Override
   public SourceVersion getSupportedSourceVersion() {
@@ -51,13 +54,17 @@ public class GrettingProcessor extends AbstractProcessor {
 
   /**
    * 프로세서가 프로세스를 진행하고, RoundEnvironment를 통해 개발자는 여러가지 상황 제약 및 처리할 수 있음
-   * @param annotations
-   * @param roundEnv
-   * @return
    */
   @Override
   public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-    for(Element annotatedElement : roundEnv.getElementsAnnotatedWith(SayHello.class)){
+
+    for (Element annotatedElement : roundEnv.getElementsAnnotatedWith(SayHello.class)) {
+
+      if (annotatedElement.getKind() != ElementKind.CLASS) {
+        error(annotatedElement, "Only classes can be annotated with @%s", SayHello.class.getSimpleName());
+        return true;
+      }
+
       TypeElement annotatedClass = (TypeElement) annotatedElement;
       String annotatedClassName = annotatedClass.getSimpleName().toString();
       System.out.printf("sdfds");
@@ -65,4 +72,7 @@ public class GrettingProcessor extends AbstractProcessor {
     return false;
   }
 
+  private void error(Element e, String msg, Object... args) {
+    messager.printMessage(Diagnostic.Kind.ERROR, String.format(msg, args), e);
+  }
 }
